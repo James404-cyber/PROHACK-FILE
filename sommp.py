e5af8f1e67f02bb9da0eeeff1fdb6038fb106c55
James404 <68128416+James404-cyber@users.noreply.github.com>
2021-12-14 20:50:37 +0300

    Add files via upload

---
+++ sommp.py
@@ -0,0 +1,13 @@
+import os, sys
+if __name__ == "__main__":
+	if len(sys.argv) == 2:
+		if sys.argv[1] == "remove":
+			os.system("rm -f .__apikey__.txt")
+			exit(" [!] Succesfull Deleted")
+		else:
+			print(" [?] Wellcome : ")
+			exit(" [!] Run : python Run.py remove")
+	try:
+		__import__("somix").__main_Somi()
+	except Exception as e:
+		exit(str(e))
