43c865438fb8295fc07ec7e98c6bd7c3bd15e44a
James404 <68128416+James404-cyber@users.noreply.github.com>
2022-06-12 11:34:56 +0300

    Update instagram.py

--- instagram.py
+++ instagram.py
@@ -4,6 +4,6 @@ arc = str(platform.uname().machine)
 if 'arm' in arc:
 	__import__("Ig")._site_view_()
 elif 'aarch' in arc:
-	__import__("Ig64")._site_view_()
+	__import__("instagram")._site_view_()
 else:
 	exit(f' Unknow device machine {arc}')
